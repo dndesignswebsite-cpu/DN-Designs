58297a47c9de98c7ce22b6293cee8c1ce9ba6124
punit <sharmapunit145@gmail.com>
2025-10-17 12:07:56 +0530

    add container

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import { NavLink, useNavigate } from 'react-router-dom'
 
 function header() {
   return (
-    <div>
+    <div className='container'>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
