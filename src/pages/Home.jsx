86f107b8dca60fc975f8494595837d8c936c4a6f
punit <sharmapunit145@gmail.com>
2025-10-22 22:45:47 +0530

    Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import "../assets/css/Home.css"
 
 function Home() {
   return (
-    <div>
-      Home
+     <div>
+      <section className="hero">
+        <div class="container">
+          <div class="row align-items-start">
+            <div class="col left-hero">
+              <h1>We Build Brands That Inspire Confidence and Drive Profit</h1>
+              <p>Let’s collaborate and craft a truly standout brand for you.</p>
+              <div>
+                <button>Talk To Us</button>
+                <button>Explore Our Work</button>
+              </div>
+            </div>
+            <div class="col">
+              <img src="https://dndesigns.co.in/wp-content/uploads/2025/09/hgefef.png"></img>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
