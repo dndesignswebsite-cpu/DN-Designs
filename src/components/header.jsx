527fae08e5ab9c4ac80d3c236be051accccfd2e4
punit <sharmapunit145@gmail.com>
2025-10-24 14:40:06 +0530

    header fixes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -58,14 +58,9 @@ function header() {
     <div>
 
             <header className="header">
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
                 <div className="logo">
                     <img src="https://dndesigns.co.in/wp-content/uploads/2022/09/dn.png" alt="DN Designs Logo" />
                 </div>
-                </div>
-                <div className="col nav-toggle-column">
                 <button
                     className={`nav-toggle-btn ${isAnimating ? 'is-active' : ''}`}
                     onClick={toggleNav}
@@ -75,9 +70,6 @@ function header() {
                     Let's Connect
                     <span className="toggle-icon"></span>
                 </button>
-                </div>
-                </div>
-                </div>
             </header>
 
             <nav 
