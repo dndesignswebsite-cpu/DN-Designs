5f74975da952a880c436c062bb66a3fb04bb872f
punit <sharmapunit145@gmail.com>
2025-10-27 18:49:49 +0530

    service

---
+++ src/pages/Service.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import "../assets/css/Service.css"
+
+function Service() {
+  return (
+    <div>
+      <section className='service-hero'>
+      <div class="container">
+  <div class="row">
+  <h1>Services</h1>
+    <div class="col-12 col-md-4">
+    {/* .. */}
+    </div>
+    <div class="col-12 col-md-8">
+      <div className='d-flex justify-content-end'>
+      <button className='talk-to-us'>Talk to Us</button>
+      </div>
+      <p>Successful brands are not made in a day. They are a result of consistent hard work, perseverance and unwavering passion. Long-term vision, strategy and creativity are pivotal too. Lots of work and lots of dedication are required. This is why you need the services of a branding & design agency like us. Let’s walk you through our services and inform you of our capabilities and approach. If there are questions in your mind, check out our FAQs section. Alternatively, reach out to us and we will promptly answer them.</p>
+    </div>
+  </div>
+</div>
+      </section>
+
+
+      {/* our services */}
+
+      <section className='our-service'>
+      <div className='container'>
+         <div className='row'>
+         <h2><span className='our'>Our</span>Services</h2>
+            <div className='col'>
+
+            </div>
+         </div>
+      </div>
+        
+      </section>
+    </div>
+  )
+}
+
+export default Service
