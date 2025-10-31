060db9b53f40f8d21a33fac865c5db95d083fc0a
punit <sharmapunit145@gmail.com>
2025-10-31 17:48:22 +0530

    home page

--- src/components/OurConstant.jsx
+++ src/components/OurConstant.jsx
@@ -181,7 +181,8 @@ function OurConstant() {
 
 
 {/* tab */}
-         <div class="container text-center tab-screen">
+
+<div class="container text-center tab-screen">
          <h2 className="our-brand-heading">Our Constant Companions</h2>
           <div class="comapnies-logo-row">
            
@@ -193,12 +194,6 @@ function OurConstant() {
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/holidayinn_logo.png"
-                className="img-fluid"
-              ></img>
-            </div>
-            <div class="companies">
-              <img
                 src="https://dndesigns.co.in/wp-content/uploads/2024/08/audi.png"
                 className="img-fluid"
               ></img>
@@ -206,6 +201,12 @@ function OurConstant() {
             <div class="companies">
               <img
                 src="https://dndesigns.co.in/wp-content/uploads/2025/06/PB_Business.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/iOrganic-Logo-NEW-web.png"
                 className="img-fluid"
               ></img>
             </div>
@@ -213,29 +214,91 @@ function OurConstant() {
           </div>
 
           <div class="comapnies-logo-row">
+
+          <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/wlues-1.png"
+                className="img-fluid"
+              ></img>
+            </div>
            
-             
-            <div class="companies">
+             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/holidayinn_logo.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/iOrganic-Logo-NEW-web-1.png"
                 className="img-fluid"
               ></img>
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2024/08/audi.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/enlite.png"
                 className="img-fluid"
               ></img>
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/PB_Business.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/nectarpure.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            
+          </div>
+
+
+          <div class="comapnies-logo-row">
+
+          <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2023/10/smartyum.png"
                 className="img-fluid"
               ></img>
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/iOrganic-Logo-NEW-web.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/Thames.png"
+                className="img-fluid"
+              ></img>
+            </div>
+           
+             <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/veikk.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/enliiv.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            
+          </div>
+
+
+          <div class="comapnies-logo-row">
+
+          <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/bobalist.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/Nature-balance.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/Untitled-1.png"
+                className="img-fluid"
+              ></img>
+            </div>
+           
+             <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/qualiteq.png"
                 className="img-fluid"
               ></img>
             </div>
@@ -243,123 +306,200 @@ function OurConstant() {
 
 
           <div class="comapnies-logo-row">
+
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/holidayinn_logo.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/rungta.png"
                 className="img-fluid"
               ></img>
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2024/08/audi.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/floris.png"
                 className="img-fluid"
               ></img>
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/PB_Business.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/david.png"
                 className="img-fluid"
               ></img>
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/iOrganic-Logo-NEW-web.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/gleephoria.png"
                 className="img-fluid"
               ></img>
             </div>
           </div>
+
 
 
           <div class="comapnies-logo-row">
            
-            <div class="companies">
+             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/holidayinn_logo.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/himalayan.png"
                 className="img-fluid"
               ></img>
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2024/08/audi.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/fluke.png"
                 className="img-fluid"
               ></img>
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/PB_Business.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2024/08/one.png"
                 className="img-fluid"
               ></img>
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/iOrganic-Logo-NEW-web.png"
-                className="img-fluid"
-              ></img>
-            </div>
-          </div>
-
-
-          <div class="comapnies-logo-row">
-            <div class="companies">
-              <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/holidayinn_logo.png"
-                className="img-fluid"
-              ></img>
-            </div>
-            <div class="companies">
-              <img
-                src="https://dndesigns.co.in/wp-content/uploads/2024/08/audi.png"
-                className="img-fluid"
-              ></img>
-            </div>
-            <div class="companies">
-              <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/PB_Business.png"
-                className="img-fluid"
-              ></img>
-            </div>
-            <div class="companies">
-              <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/iOrganic-Logo-NEW-web.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/mr-bomzy.png"
                 className="img-fluid"
               ></img>
             </div>
           </div>
         </div>
-
 
 
 
 {/* mobile */}
 
-         <div class="container text-center mobile-screen px-3">
+
+<div class="container text-center mobile-screen">
          <h2 className="our-brand-heading">Our Constant Companions</h2>
           <div class="comapnies-logo-row">
+           
+             <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/holidayinn_logo.png"
+                className="img-fluid"
+              ></img>
+            </div>
             <div class="companies">
               <img
                 src="https://dndesigns.co.in/wp-content/uploads/2024/08/audi.png"
                 className="img-fluid"
               ></img>
             </div>
+            </div>
+
+            <div class="comapnies-logo-row">
             <div class="companies">
               <img
                 src="https://dndesigns.co.in/wp-content/uploads/2025/06/PB_Business.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/iOrganic-Logo-NEW-web.png"
                 className="img-fluid"
               ></img>
             </div>
             
           </div>
 
+
           <div class="comapnies-logo-row">
+
+          <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/wlues-1.png"
+                className="img-fluid"
+              ></img>
+            </div>
+           
+             <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/iOrganic-Logo-NEW-web-1.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            </div>
+<div class="comapnies-logo-row">
+
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/PB_Business.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/enlite.png"
                 className="img-fluid"
               ></img>
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/iOrganic-Logo-NEW-web.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/nectarpure.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            
+          </div>
+
+
+          <div class="comapnies-logo-row">
+
+          <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2023/10/smartyum.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/Thames.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            </div>
+
+            <div class="comapnies-logo-row">
+           
+             <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/veikk.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/enliiv.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            
+          </div>
+
+
+          <div class="comapnies-logo-row">
+
+          <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/bobalist.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/Nature-balance.png"
+                className="img-fluid"
+              ></img>
+            </div>
+
+            </div>
+
+            <div class="comapnies-logo-row">
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/06/Untitled-1.png"
+                className="img-fluid"
+              ></img>
+            </div>
+           
+             <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/qualiteq.png"
                 className="img-fluid"
               ></img>
             </div>
@@ -367,55 +507,72 @@ function OurConstant() {
 
 
           <div class="comapnies-logo-row">
-          
+
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/PB_Business.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/rungta.png"
                 className="img-fluid"
               ></img>
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/iOrganic-Logo-NEW-web.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/floris.png"
+                className="img-fluid"
+              ></img>
+            </div>
+
+            </div>
+
+            <div class="comapnies-logo-row">
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/david.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/gleephoria.png"
                 className="img-fluid"
               ></img>
             </div>
           </div>
 
 
-          <div class="comapnies-logo-row">
-        
-            <div class="companies">
-              <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/PB_Business.png"
-                className="img-fluid"
-              ></img>
-            </div>
-            <div class="companies">
-              <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/iOrganic-Logo-NEW-web.png"
-                className="img-fluid"
-              ></img>
-            </div>
-          </div>
-
 
           <div class="comapnies-logo-row">
-          
-            <div class="companies">
+           
+             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/PB_Business.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/himalayan.png"
                 className="img-fluid"
               ></img>
             </div>
             <div class="companies">
               <img
-                src="https://dndesigns.co.in/wp-content/uploads/2025/06/iOrganic-Logo-NEW-web.png"
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/fluke.png"
+                className="img-fluid"
+              ></img>
+            </div>
+
+            </div>
+
+            <div class="comapnies-logo-row">
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2024/08/one.png"
+                className="img-fluid"
+              ></img>
+            </div>
+            <div class="companies">
+              <img
+                src="https://dndesigns.co.in/wp-content/uploads/2025/07/mr-bomzy.png"
                 className="img-fluid"
               ></img>
             </div>
           </div>
         </div>
+
       </section>
 
     </div>
