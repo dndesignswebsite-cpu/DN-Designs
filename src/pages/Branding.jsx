f93d65162a577452c5afd98830389fc896d529ae
punit <sharmapunit145@gmail.com>
2025-10-31 10:53:07 +0530

    branding page

--- src/pages/Branding.jsx
+++ src/pages/Branding.jsx
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
 import "../assets/css/Branding.css"
  import HorizontalScroll from '../components/HorizontalScroll'
  import OurConstant from "../components/OurConstant"
 
 function Branding() {
-
+  // faqs
+  const [activeIndex_f, setActiveIndex_f] = useState(null);
+    const [activeIndex_s, setActiveIndex_s] = useState(null);
+  
+    const toggleAccordion_f = (index) => {
+      if (activeIndex_f === index) {
+        setActiveIndex_f(null);
+      } else {
+        setActiveIndex_f(index);
+      }
+    };
+  
+    const toggleAccordion_s = (index) => {
+      if (activeIndex_s === index) {
+        setActiveIndex_s(null);
+      } else {
+        setActiveIndex_s(index);
+      }
+    };
+  
+    const sheet_one = [
+      {
+        title: "What services does DN Designs Offer",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      },
+      {
+        title: " do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        content:
+          "Ut enim ad minim veniam, quis nostrud do eiusmod tempor incididunt ut labore et dolore magna aliqua. exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      },
+      {
+        title: " do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        content:
+          "Duis aute irure dolor in do eiusmod tempor incididunt ut labore et dolore magna aliqua. reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      },
+    ];
+  
+    const sheet_two = [
+      {
+        title:
+          "Section do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        content:
+          "Lorem ipsum dolor do eiusmod tempor incididunt ut labore et dolore magna aliqua. sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      },
+      {
+        title:
+          "Section do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        content:
+          "Ut enim ad minim  do eiusmod tempor incididunt ut labore et dolore magna aliqua.veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      },
+      {
+        title:
+          "Section  do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        content:
+          "Duis aute irure do eiusmod tempor incididunt ut labore et dolore magna aliqua. dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      },
+    ];
+  
+// horizontal scroll
   const slidesData = [
     { title: "The Bobalist", img: 'https://dndesigns.co.in/wp-content/uploads/2025/06/14.jpg',
        para:"For Rithm’s Enlite, a brand with sparkling mineral water For Rithm’s Enlite, a brand with sparkling mineral water For Rithm’s Enlite, a brand with sparkling mineral water For Rithm’s Enlite, a brand with sparkling mineral water For Rithm’s Enlite, a brand with sparkling mineral water For Rithm’s Enlite, a brand with sparkling mineral water For Rithm’s Enlite, a brand with sparkling mineral water "
@@ -183,121 +242,6 @@ function Branding() {
       </section>
 
 
-{/* branding that
-      <section className='branding-that'>
-      <div className='container'>
-        <h2 className="text-center">
-              Our<span className="every-pr"> Work Portfolio</span>
-            </h2>
-
-            
-            <div className='branding-that-div'>
-                <img src='https://dndesigns.co.in/wp-content/uploads/2025/06/13.jpg'
-                className='img-fluid'
-                ></img>
-                   <div className="brand-that-overlay">
-                  <div className="our-brand-that-content">
-                    <h3>Grin Care</h3>
-                    <div className="brand-that-buttons">
-                      <button>Brand Identity</button>
-                      <button>UI/UX</button>
-                      <button>Website</button>
-                      <button>Website</button>
-                    </div>
-                    <p>
-                      For Rithm’s Enlite, a brand with sparkling mineral water
-                      and prebiotic drink range, we designed a thoughtful and
-                      eye-catching brand identity, including can design, logo
-                      design and character design. We created the character and
-                      the overall brand design around the brand name to promote
-                      the refreshing and calming properties of the product.
-                    </p>
-                  </div>
-                </div>
-            </div>
-
-
-            
-            <div className='branding-that-div'>
-                <img src='https://dndesigns.co.in/wp-content/uploads/2025/06/13.jpg'
-                className='img-fluid'
-                ></img>
-                   <div className="brand-that-overlay">
-                  <div className="our-brand-that-content">
-                    <h3>Grin Care</h3>
-                    <div className="brand-that-buttons">
-                      <button>Brand Identity</button>
-                      <button>UI/UX</button>
-                      <button>Website</button>
-                      <button>Website</button>
-                    </div>
-                    <p>
-                      For Rithm’s Enlite, a brand with sparkling mineral water
-                      and prebiotic drink range, we designed a thoughtful and
-                      eye-catching brand identity, including can design, logo
-                      design and character design. We created the character and
-                      the overall brand design around the brand name to promote
-                      the refreshing and calming properties of the product.
-                    </p>
-                  </div>
-                </div>
-            </div>
-
-           
-            <div className='branding-that-div'>
-                <img src='https://dndesigns.co.in/wp-content/uploads/2025/06/13.jpg'
-                className='img-fluid'
-                ></img>
-                   <div className="brand-that-overlay">
-                  <div className="our-brand-that-content">
-                    <h3>Grin Care</h3>
-                    <div className="brand-that-buttons">
-                      <button>Brand Identity</button>
-                      <button>UI/UX</button>
-                      <button>Website</button>
-                      <button>Website</button>
-                    </div>
-                    <p>
-                      For Rithm’s Enlite, a brand with sparkling mineral water
-                      and prebiotic drink range, we designed a thoughtful and
-                      eye-catching brand identity, including can design, logo
-                      design and character design. We created the character and
-                      the overall brand design around the brand name to promote
-                      the refreshing and calming properties of the product.
-                    </p>
-                  </div>
-                </div>
-            </div>
-
-            
-
-            <div className='branding-that-div'>
-                <img src='https://dndesigns.co.in/wp-content/uploads/2025/06/13.jpg'
-                className='img-fluid'
-                ></img>
-                   <div className="brand-that-overlay">
-                  <div className="our-brand-that-content">
-                    <h3>Grin Care</h3>
-                    <div className="brand-that-buttons">
-                      <button>Brand Identity</button>
-                      <button>UI/UX</button>
-                      <button>Website</button>
-                      <button>Website</button>
-                    </div>
-                    <p>
-                      For Rithm’s Enlite, a brand with sparkling mineral water
-                      and prebiotic drink range, we designed a thoughtful and
-                      eye-catching brand identity, including can design, logo
-                      design and character design. We created the character and
-                      the overall brand design around the brand name to promote
-                      the refreshing and calming properties of the product.
-                    </p>
-                  </div>
-                </div>
-            </div>
-            
-      </div>
-      </section> */}
 
 {/* branding that */}
 
@@ -433,58 +377,110 @@ function Branding() {
                   </div>
             </li>
 
-              <li class="card-create" id="card2-create">
-              <div class="card-body-create">
-                   <div class="card-body-create row">
-                   <div className='col-2 create-number'>01</div>
+            
+
+            <li class="card-create" id="card2-create">
+              <div class="card-body-create row">
+                   <div className='col-2 create-number'>02</div>
                    <div className='col-10'>
-                    <h2>Brand Understanding</h2>
-                    <p>If anything has to stand the test of time, it needs to begin with a firm footing. In branding, this beginning is understanding the product/business. We sit down with you for a deep discussion to understand your product or service as well as your vision and mission. This helps us make a good start and prepares us for an exciting journey ahead.</p>
+                    <h2>Competitor Analysis</h2>
+                    <p>You are not alone in the market; there are several other products in the same category out there trying to build their presence. How do you craft your own little corner? Analysing competition is crucial, and this is what we, as a brand design company, do next. We conduct a thorough research of your competitors and understand your current position in the market to ensure that we have enough knowledge and data to take a step forward, creating your branding strategy.</p>
                    </div>
-                  </div>
                   </div>
             </li>
 
-              <li class="card-create" id="card3-create">
-              <div class="card-body-create">
-                   <div class="card-body-create row">
-                   <div className='col-2 create-number'>01</div>
+               <li class="card-create" id="card3-create">
+              <div class="card-body-create row">
+                   <div className='col-2 create-number'>03</div>
                    <div className='col-10'>
-                    <h2>Brand Understanding</h2>
-                    <p>If anything has to stand the test of time, it needs to begin with a firm footing. In branding, this beginning is understanding the product/business. We sit down with you for a deep discussion to understand your product or service as well as your vision and mission. This helps us make a good start and prepares us for an exciting journey ahead.</p>
+                    <h2>Planning Your Brand</h2>
+                    <p>It’s now time to create your brand strategy. Every aspect of branding is carefully thought out and discussed with you - be it your brand personality, story, message, values, or even communication and website design. As a creative branding agency, we decide on these elements based on how we want the audience to perceive your brand.</p>
                    </div>
-                  </div>
                   </div>
             </li>
 
-             <li class="card-create" id="card4-create">
-              <div class="card-body-create">
-                    <div class="card-body-create row">
-                   <div className='col-2 create-number'>01</div>
+            <li class="card-create" id="card4-create">
+              <div class="card-body-create row">
+                   <div className='col-2 create-number'>04</div>
                    <div className='col-10'>
-                    <h2>Brand Understanding</h2>
-                    <p>If anything has to stand the test of time, it needs to begin with a firm footing. In branding, this beginning is understanding the product/business. We sit down with you for a deep discussion to understand your product or service as well as your vision and mission. This helps us make a good start and prepares us for an exciting journey ahead.</p>
+                    <h2>Implementing the Strategy</h2>
+                    <p>Let’s turn words and ideas into action. We now sit down to do the actual work - naming your brand, designing your logo, creating your packaging design, crafting a tagline, composing your message, and much more. In addition, we design and develop your website too.</p>
                    </div>
-                  </div>
                   </div>
             </li>
 
 
-              <li class="card-create" id="card5-create">
-              <div class="card-body-create">
-                  <div class="card-body-create row">
-                   <div className='col-2 create-number'>01</div>
+               <li class="card-create" id="card5-create">
+              <div class="card-body-create row">
+                   <div className='col-2 create-number'>05</div>
                    <div className='col-10'>
-                    <h2>Brand Understanding</h2>
-                    <p>If anything has to stand the test of time, it needs to begin with a firm footing. In branding, this beginning is understanding the product/business. We sit down with you for a deep discussion to understand your product or service as well as your vision and mission. This helps us make a good start and prepares us for an exciting journey ahead.</p>
+                    <h2>Feedback & Launch</h2>
+                    <p>The final stage in our branding journey, this is where we seek a review of our work. Based on your feedback, we make changes, and reseek your feedback. Once we receive a green light from you, we go ahead and launch your brand.</p>
                    </div>
                   </div>
-                  </div>
-            </li>  
+            </li> 
             </ul>
             </div>
             </section>
-<OurConstant/>
+
+              {/* faqs */}
+                <section className="faqs">
+  <div className="container">
+    <div className="row">
+      <h2 className="text-center">Frequently Asked Questions</h2>
+
+      <div className="col-12 col-md-12 col-lg-6">
+        <div className="all_faqs">
+          {sheet_one.map((first_sheet, index) => (
+            <div className="faq_unit" key={index}>
+              <button
+                className={`accordion ${
+                  activeIndex_f === index ? "active" : ""
+                }`}
+                onClick={() => toggleAccordion_f(index)}
+              >
+                {first_sheet.title}
+              </button>
+              <div
+                className={`panel ${
+                  activeIndex_f === index ? "open" : ""
+                }`}
+              >
+                <p>{first_sheet.content}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="col-12 col-md-12 col-lg-6">
+        <div className="all_faqs">
+          {sheet_two.map((second_sheet, index) => (
+            <div className="faq_unit" key={index}>
+              <button
+                className={`accordion ${
+                  activeIndex_s === index ? "active" : ""
+                }`}
+                onClick={() => toggleAccordion_s(index)}
+              >
+                {second_sheet.title}
+              </button>
+              <div
+                className={`panel ${
+                  activeIndex_s === index ? "open" : ""
+                }`}
+              >
+                <p>{second_sheet.content}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
     </div>
   )
 }
