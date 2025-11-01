5cb9748cc478fbe70c2f34a18f1fd9170c0c55af
punit <sharmapunit145@gmail.com>
2025-11-01 16:48:32 +0530

    home page mobile responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -541,19 +541,19 @@ function Home() {
     <div class="col-3 points-left">
       <img src='https://dndesigns.co.in/wp-content/uploads/2025/06/350x1100.jpg'/>
     </div>
-    <div class="col-9 points-right">
+    <div class="col-12 col-md-12 col-lg-9 points-right">
     <div className='points row'>
-      <div className="col-2">
+      <div className="col-12 col-md-12 col-lg-2">
       <h2>1</h2>
       </div>
-      <div className="col-3">
+      <div className="col-12 col-md-12 col-lg-3">
       <h3>Branding pages</h3>
       </div>
-      <div className='points-para col-4'>
+      <div className='points-para col-12 col-md-12 col-lg-4'>
         <p>It is a significant challenge to create a space for your product in the market. It requires strategic thinking, creative skills, and a relentless pursuit of goals.</p>
         <p>Worried? Don’t be, for we are here.</p>
       </div>
-       <div className='points-para col-3'>
+       <div className='points-para col-12 col-md-12 col-lg-3'>
         <p>We provide end-to-end branding services.</p>
         <p>  Brand Identity Design
   Packaging Design
@@ -562,34 +562,34 @@ function Home() {
     </div>
 
       <div className='points row Points-Comm'>
-        <div className="col-2">
+        <div className="col-12 col-md-12 col-lg-2">
       <h2>2</h2>
       </div>
-       <div className="col-3">
+       <div className="col-12 col-md-12 col-lg-3">
       <h3>Communication Strategy</h3>
       </div>
-      <div className='points-para col-4'>
+      <div className='points-para col-12 col-md-12 col-lg-4'>
         <p>Customers must be aware of your excellent product. Communication, therefore, is crucial to establishing and strengthening your brand’s presence in the market.</p>
         <p>What are you waiting for then? Connect with us today.</p>
       </div>
-       <div className='points-para col-3'>
+       <div className='points-para col-12 col-md-12 col-lg-3'>
         <p>It is a significant challenge to create a space for your product in the market. It requires strategic thinking, creative skills, and a relentless pursuit of goals.</p>
         <p>Worried? Don’t be, for we are here.</p>
       </div>
     </div>
 
       <div className='points row'>
-        <div className="col-2">
+        <div className="col-12 col-md-12 col-lg-2">
       <h2>3</h2>
       </div>
-       <div className="col-3">
+       <div className="col-12 col-md-12 col-lg-3">
       <h3>Web Design</h3>
       </div>
-      <div className='points-para col-4'>
+      <div className='points-para col-12 col-md-12 col-lg-4'>
         <p>It is a significant challenge to create a space for your product in the market. It requires strategic thinking, creative skills, and a relentless pursuit of goals.</p>
         <p>Worried? Don’t be, for we are here.</p>
       </div>
-       <div className='points-para col-3'>
+       <div className='points-para col-12 col-md-12 col-lg-3'>
         <p>It is a significant challenge to create a space for your product in the market. It requires strategic thinking, creative skills, and a relentless pursuit of goals.</p>
         <p>Worried? Don’t be, for we are here.</p>
       </div>
