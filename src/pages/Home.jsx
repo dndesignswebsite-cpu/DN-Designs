cc7ee3f286c688d70011c844d27766fa90ed8c1d
Shivam-dndesigns <website.dndesigns@gmail.com>
2025-11-01 15:33:59 +0530

    home page alignment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -534,22 +534,26 @@ function Home() {
       </section>
 
       {/*.....points...... */}
-      {/* 
+      
 <section className='point'>
-<div class="container">
-  <div class="row g-0 align-items-center points-row">
-    <div class="col-4 points-left">
-      <img src='https://dndesigns.co.in/wp-content/uploads/2025/06/350x1100.jpg' className='img-fluid'/>
+<div class="container point-cont">
+  <div class="row points-row">
+    <div class="col-3 points-left">
+      <img src='https://dndesigns.co.in/wp-content/uploads/2025/06/350x1100.jpg'/>
     </div>
-    <div class="col-8 points-right">
-    <div className='points'>
+    <div class="col-9 points-right">
+    <div className='points row'>
+      <div className="col-2">
       <h2>1</h2>
+      </div>
+      <div className="col-3">
       <h3>Branding</h3>
-      <div className='points-para'>
+      </div>
+      <div className='points-para col-4'>
         <p>It is a significant challenge to create a space for your product in the market. It requires strategic thinking, creative skills, and a relentless pursuit of goals.</p>
         <p>Worried? Don’t be, for we are here.</p>
       </div>
-       <div className='points-para'>
+       <div className='points-para col-3'>
         <p>We provide end-to-end branding services.</p>
         <p>  Brand Identity Design
   Packaging Design
@@ -557,27 +561,35 @@ function Home() {
       </div>
     </div>
 
-      <div className='points'>
+      <div className='points row Points-Comm'>
+        <div className="col-2">
       <h2>2</h2>
+      </div>
+       <div className="col-3">
       <h3>Communication Strategy</h3>
-      <div className='points-para'>
+      </div>
+      <div className='points-para col-4'>
         <p>Customers must be aware of your excellent product. Communication, therefore, is crucial to establishing and strengthening your brand’s presence in the market.</p>
         <p>What are you waiting for then? Connect with us today.</p>
       </div>
-       <div className='points-para'>
+       <div className='points-para col-3'>
         <p>It is a significant challenge to create a space for your product in the market. It requires strategic thinking, creative skills, and a relentless pursuit of goals.</p>
         <p>Worried? Don’t be, for we are here.</p>
       </div>
     </div>
 
-      <div className='points'>
+      <div className='points row'>
+        <div className="col-2">
       <h2>3</h2>
+      </div>
+       <div className="col-3">
       <h3>Web Design</h3>
-      <div className='points-para'>
+      </div>
+      <div className='points-para col-4'>
         <p>It is a significant challenge to create a space for your product in the market. It requires strategic thinking, creative skills, and a relentless pursuit of goals.</p>
         <p>Worried? Don’t be, for we are here.</p>
       </div>
-       <div className='points-para'>
+       <div className='points-para col-3'>
         <p>It is a significant challenge to create a space for your product in the market. It requires strategic thinking, creative skills, and a relentless pursuit of goals.</p>
         <p>Worried? Don’t be, for we are here.</p>
       </div>
@@ -586,7 +598,7 @@ function Home() {
   </div>
 </div>
 </section>
- */}
+
 
       {/*.....testimonial...... */}
 
